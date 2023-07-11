52324d7ef800a6e5b5263742d4646fe1cef6c369
Ascuh <ascuneagustin@gmail.com>
2023-07-11 11:46:15 -0300

    movimiento naranja

---
+++ proyecto 4D/Assets/scripts/camara/PlayerCam.cs
@@ -0,0 +1,45 @@
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerCam : MonoBehaviour
+{
+    public float sensX;
+    public float sensY;
+
+    public Transform orientation;
+
+    float xRotation;
+    float yRotation;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // get mouse input
+
+        float mouseX = Input.GetAxisRaw("Mouse X") * Time.deltaTime * sensX;
+        float mouseY = Input.GetAxisRaw("Mouse Y") * Time.deltaTime * sensY;
+
+
+        yRotation += mouseX;
+
+        xRotation -= mouseY;
+
+        //para que la camara no se mueva mas de 90 grados para arriba ni 90 grados para abajo
+        xRotation = Mathf.Clamp(xRotation, -90f, 90f);
+
+        //rotate cam and orientation 
+        transform.rotation = Quaternion.Euler(xRotation, yRotation, 0);
+        orientation.rotation = Quaternion.Euler(0, yRotation, 0);
+
+    }
+}
