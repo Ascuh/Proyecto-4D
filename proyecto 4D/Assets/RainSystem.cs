205a526979f24a9fb6e1415e5bc9694751a4b095
Ascuh <ascuneagustin@gmail.com>
2023-08-03 08:33:34 -0300

    nodnadawd

---
+++ proyecto 4D/Assets/RainSystem.cs
@@ -0,0 +1,33 @@
+ using System.Collections;
+using System.Collections.Generic;
+using UnityEditor.ShaderGraph.Internal;
+using UnityEngine;
+
+public class RainSystem : MonoBehaviour
+{
+    public ParticleSystem rain;
+    float rate = 0;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        var rainEmission = rain.emission;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        startRain(2000f);
+    }
+
+    void startRain(float maxRain)
+    {
+        if (rate < maxRain)
+        {
+            rate += 1;
+        }
+
+        var rainEmission = rain.emission;
+        rainEmission.rateOverTime = rate;
+    }
+}
