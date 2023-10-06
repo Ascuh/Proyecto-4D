647fb58ffe2c480a9fb49b7676eb4666126c74b4
Ascuh <ascuneagustin@gmail.com>
2023-10-06 11:06:48 -0300

    niebla y cueva
    se desactiva la niebla cuando entras, se activa cuando salis, tiembla la camra cuando pasas por el cartel de danger y cuanto mas te acercas mas tiembla

--- Assets/scripts/naturaleza/derrumbe.cs
+++ Assets/scripts/naturaleza/derrumbe.cs
@@ -6,7 +6,17 @@ public class derrumbe : MonoBehaviour
 {
     public CamaraSacudida camaraSacudida;
     public GameObject piedras;
-    bool simonLikerMiCorazonUwU;
+    bool derrumbo;
+    bool tiemblaPoco;
+    bool tiemblaMucho;
+
+    public string derrumb = "Derrumbe";
+    public string tiembPoco = "tiemblaPoco";
+    public string tiembMucho = "tiemblaMucho";
+    public string muert = "Muerte";
+    public string saliCueva = "salidaCueva";
+
+    public AdministradorEscenas adminEscenas;
 
     // Start is called before the first frame update
     void Start()
@@ -20,13 +30,70 @@ public class derrumbe : MonoBehaviour
         
     }
 
-    private void OnTriggerEnter(Collider other)
+    private void OnTriggerExit(Collider other)
     {
-        if (!simonLikerMiCorazonUwU)
+        if (other.CompareTag(derrumb))
         {
-            simonLikerMiCorazonUwU = true;
+            Derrumbe();
+        }
+        if (other.CompareTag(tiembPoco))
+        {
+            TiemblaPoco();
+        }
+        if (other.CompareTag(tiembMucho))
+        {
+            TiemblaMucho();
+        }
+        if (other.CompareTag(muert))
+        {
+            adminEscenas.MainMenu();
+        }
+        if (other.CompareTag(saliCueva))
+        {
+            SalidaCueva();
+        }
+
+    }
+
+    private void Derrumbe()
+    {
+        if (!derrumbo)
+        {
+            derrumbo = true;
             piedras.SetActive(true);
             camaraSacudida.IniciarSacudida(3, 0.5f);
+            RenderSettings.fog = false;
         }
+    }
+    private void TiemblaPoco()
+    {
+        if (!tiemblaPoco)
+        {
+            tiemblaPoco = true;
+            camaraSacudida.IniciarSacudida(100, 0.02f);
+        }
+        else
+        {
+            tiemblaPoco = false;
+            camaraSacudida.IniciarSacudida(0, 0);
+        }
+    }
+    private void TiemblaMucho()
+    {
+        if (!tiemblaMucho)
+        {
+            tiemblaMucho = true;
+            camaraSacudida.IniciarSacudida(100, 0.08f);
+        }
+        else
+        {
+            tiemblaMucho = false;
+            camaraSacudida.IniciarSacudida(100, 0.02f);
+        }
+    }
+    private void SalidaCueva()
+    {
+        piedras.SetActive(false);
+        RenderSettings.fog = true;
     }
 }
