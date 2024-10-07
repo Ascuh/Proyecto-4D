15eb583b19f02ae034dc88618f19f4917125c829
Ascuh <ascuneagustin@gmail.com>
2024-10-07 00:36:38 -0300

    las arañas te matan
    sonidos incluidos

---
+++ Assets/scripts/spider.cs
@@ -0,0 +1,54 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class spider : MonoBehaviour
+{
+
+
+    private NavMeshAgent agent;  // Referencia al NavMeshAgent
+    private Transform player;     // Referencia al transform del jugador
+    spiderManager SpiderManager;
+
+    void Start()
+    {
+        // Obtiene el componente NavMeshAgent
+        agent = GetComponent<NavMeshAgent>();
+
+        // Busca al jugador por su tag
+        player = GameObject.FindGameObjectWithTag("Player").transform;
+
+        // Obtiene el componente SpiderManager en el mismo objeto o en el padre
+        SpiderManager = FindObjectOfType<spiderManager>();
+        if (SpiderManager == null)
+        {
+            Debug.LogError("No se encontró el componente SpiderManager en este objeto.");
+        }
+    }
+
+    void Update()
+    {
+        if (player != null)
+        {
+            // Mueve a la araña hacia el jugador
+            agent.SetDestination(player.position);
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        // Verifica si colisiona con el jugador
+        if (other.CompareTag("Player"))
+        {
+            LaArañaTeAtaco();
+            // Destruye la araña
+            Destroy(gameObject);
+        }
+    }
+
+    private void LaArañaTeAtaco()
+    {
+        SpiderManager.arañasPegadas++;
+    }
+}
