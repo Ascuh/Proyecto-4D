9cfa57b39fcef44a8d6bc4aac66e9f27eb4f5a34
Ascuh <ascuneagustin@gmail.com>
2024-10-15 16:21:00 -0300

    electricidad taser, sonido taser y arreglo de luz monstruos

--- Assets/scripts/taser.cs
+++ Assets/scripts/taser.cs
@@ -4,15 +4,42 @@ using UnityEngine;
 
 public class taser : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    // Referencias a los GameObjects
+    public GameObject ruidoTaser;
+    public GameObject efectoTaser;
 
-    // Update is called once per frame
+    public float cooldownLighting;  // Tiempo de espera
+    public float cooldownGun;  // Tiempo de espera
+
+    // Variable para controlar si el taser está en cooldown
+    private bool puedeUsarTaser = true;
+
     void Update()
     {
-        
+        // Si se presiona el click izquierdo y no está en cooldown
+        if (Input.GetMouseButtonDown(0) && puedeUsarTaser)
+        {
+            puedeUsarTaser = false;
+
+            // Activar el GameObject del ruidoTaser
+            ruidoTaser.SetActive(true);
+            efectoTaser.SetActive(true);
+
+            // Iniciar cooldown
+            StartCoroutine(IniciarCooldownLighting());
+            StartCoroutine(IniciarCooldownGun());
+        }
+    }
+
+    IEnumerator IniciarCooldownLighting()
+    {
+        yield return new WaitForSeconds(cooldownLighting);  // Esperar ? segundos
+        efectoTaser.SetActive(false);  // Desactivar el ruidoTaser después del cooldown
+    }
+    IEnumerator IniciarCooldownGun()
+    {
+        yield return new WaitForSeconds(cooldownGun);  // Esperar ? segundos
+        ruidoTaser.SetActive(false);  // Desactivar el ruidoTaser después del cooldown
+        puedeUsarTaser = true;  // Permitir el uso nuevamente
     }
 }
